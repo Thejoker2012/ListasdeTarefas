ae12181fd3597defc9942d0e17c7cdb6abd02487
Iago <iagofalcom@hotmail.com>
2019-06-30 23:39:19 -0300

    Criação do Menu
    *onCreateOptionsMenu(Menu menu)
    *getMenuInflater().inflate(R.menu.menu_adicionar_tarefa,menu)
    *switch (item.getItemId())
    *Criando ícone check
    *Criando menu_adicionar_tarefas

--- app/src/main/java/br/com/unisys/listasdetarefas/activity/AdicionarTarefaActivity.java
+++ app/src/main/java/br/com/unisys/listasdetarefas/activity/AdicionarTarefaActivity.java
@@ -1,8 +1,12 @@
 package br.com.unisys.listasdetarefas.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import br.com.unisys.listasdetarefas.R;
 
@@ -12,5 +16,24 @@ public class AdicionarTarefaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adicionar_tarefa);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_adicionar_tarefa,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.itemSalvar:
+                Toast.makeText(getApplicationContext(), "Botao adicionar pressionado", Toast.LENGTH_LONG).show();
+                //Executar acão
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
